093d20cd04da3b314abef7dbf6fcbf0e1e4e3d3f
danh-real <htduyanh210604@gmail.com>
2026-01-18 21:53:04 +0000

    fssfam

--- scripts/train_msd01_1shot.sh
+++ scripts/train_msd01_1shot.sh
@@ -33,11 +33,11 @@ else
   exit 1
 fi
 
-python train.py \
+CUDA_VISIBLE_DEVICES=1 python train.py \
     --config=${config} \
     --arch=${arch} \
     --num_refine=3 \
     --ver_refine=v1 \
     --ver_dino=$ver_dino \
-    --distributed \
     2>&1 | tee ${result_dir}/train-$now.log
+    # --distributed \
