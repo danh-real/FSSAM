aef30ce4c969102cf642cb9d7f8f910cac382f19
danh-real <htduyanh210604@gmail.com>
2026-01-24 09:09:23 +0000

    fssam

--- test.sh
+++ test.sh
@@ -7,17 +7,8 @@ bash scripts/test/test_msd_task03_5shot.sh
 bash scripts/test/test_msd_task04_1shot.sh
 bash scripts/test/test_msd_task04_5shot.sh
 
-bash scripts/test/test_msd_task05_1shot.sh
-bash scripts/test/test_msd_task05_5shot.sh
-
-bash scripts/test/test_msd_task06_1shot.sh
-bash scripts/test/test_msd_task06_5shot.sh
-
 bash scripts/test/test_msd_task07_1shot.sh
 bash scripts/test/test_msd_task07_5shot.sh
 
 bash scripts/test/test_msd_task08_1shot.sh
 bash scripts/test/test_msd_task08_5shot.sh
-
-bash scripts/test/test_msd_task09_1shot.sh
-bash scripts/test/test_msd_task09_5shot.sh
