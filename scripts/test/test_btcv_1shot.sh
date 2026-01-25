a78c0a28c6118abd754b1cec92d5f7b15fc1c6ca
danh-real <htduyanh210604@gmail.com>
2026-01-25 04:50:50 +0000

    fssam

--- scripts/test/test_btcv_1shot.sh
+++ scripts/test/test_btcv_1shot.sh
@@ -16,26 +16,6 @@ fi
 config=config/${dataset}/${net}/${dataset}_${exp_name}_${net}_${postfix}.yaml
 
 declare -a ckpt=(
-  train_epoch_0_0.1193.pth
-  train_epoch_1_0.1338.pth
-  train_epoch_2_0.1381.pth
-  train_epoch_3_0.1420.pth
-  train_epoch_4_0.1459.pth
-  train_epoch_5_0.1473.pth
-  train_epoch_6_0.1496.pth
-  train_epoch_7_0.1527.pth
-  train_epoch_8_0.1532.pth
-  train_epoch_9_0.1559.pth
-  train_epoch_10_0.1578.pth
-  train_epoch_11_0.1602.pth
-  train_epoch_12_0.1623.pth
-  train_epoch_13_0.1650.pth
-  train_epoch_14_0.1679.pth
-  train_epoch_15_0.1697.pth
-  train_epoch_16_0.1721.pth
-  train_epoch_17_0.1735.pth
-  train_epoch_18_0.1764.pth
-  train_epoch_19_0.1773.pth
   train_epoch_21_0.1807.pth
 )
 
