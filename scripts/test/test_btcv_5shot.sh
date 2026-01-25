47379119c9ddd5d41d83b439e4e4f1d758002e1d
danh-real <htduyanh210604@gmail.com>
2026-01-25 04:41:06 +0000

    fssam

---
+++ scripts/test/test_btcv_5shot.sh
@@ -0,0 +1,31 @@
+dataset=btcv  # pascal/coco
+exp_name=split0  # 0/1/2/3
+shot=5  # 1/5
+arch=FSSAM5s  # FSSAM
+net=tiny  # small
+
+if [ $shot -eq 1 ]; then
+  postfix=batch
+elif [ $shot -eq 5 ]; then
+  postfix=5s_batch
+else
+  echo "Only 1 and 5 shot are supported"
+  exit 1
+fi
+
+config=config/${dataset}/${net}/${dataset}_${exp_name}_${net}_${postfix}.yaml
+
+declare -a ckpt=(
+  train5_epoch_27_0.3258.pth
+)
+
+for weight in ${ckpt[@]};
+do
+  python test.py \
+    --config=${config} \
+    --arch=${arch} \
+    --num_refine=3 \
+    --ver_refine=v1 \
+    --ver_dino=dinov2_vitb14 \
+    --opts Test_Finetune.weight $weight
+done
